77c87d5993e296b06f63b0bb696d5da112bd3d76
pelletii <73187465+pelletii@users.noreply.github.com>
2025-10-17 07:51:34 +0000

    ajout exo02

---
+++ exoDecouverte02.php
@@ -0,0 +1,34 @@
+<doctype html>
+<html lang="fr">        
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head> 
+<body>
+    <h1>Découverte de la POO en PHP</h1>
+    <h2>Exercice 2</h2>
+
+
+</body>
+</html>
+<?php
+class CompteBancaire {
+    public $titulaire;
+    public $solde;
+
+    // Constructeur
+    public function __construct($titulaire, $soldeInitial) {
+        $this->titulaire = $titulaire;
+        $this->solde = $soldeInitial;
+    }
+
+    public function deposer($montant) {
+        $this->solde += $montant;
+        echo "Dépôt de $montant €, nouveau solde : $this->solde €<br>";
+    }
+}
+$monCompte = new CompteBancaire("Alice", 1000);
+$monCompte->deposer(200);
+?>
